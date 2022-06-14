7db9783c0c517b007d950509bbc83ac9b2d8e1c2
pgentil <pgentil@udesa.edu.ar>
2022-06-14 16:54:11 -0300

    linespace

--- plot1.py
+++ plot1.py
@@ -2,5 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x = np.linspace(0, 20, 100)
-plt.plot(x, np.sin(x))
+y = np.linspace(20, 40, 100)
+z = np.linspace(40, 60, 100)
+plt.plot(x, 800 * np.sin(x))
+plt.plot(y, 800 * np.cos(y))
+plt.plot(z, np.tan(z))
 plt.show()
+
+
