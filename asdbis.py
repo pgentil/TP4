2b7c09fbd842c0411247e29dc689082e1dad4dbd
ArcaloJade <102890895+ArcaloJade@users.noreply.github.com>
2022-06-16 11:11:47 -0300

    :)

--- asdbis.py
+++ asdbis.py
@@ -3,126 +3,108 @@ import matplotlib.pyplot as plt
 
 pi = np.pi
 
-class Attack():
+class Function():
+    
+    def __init__(self):
+        self.attack = None
+        self.sustain = None
+        self.decay = None
         
     def LINEAR(self, t0, duration, frec):
         values = np.zeros(duration)
-        for t in np.linspace(0, duration, frec):
+        for t in np.arange(0, duration, 1/frec):
             values[t] = t / t0
-        return values
+        self.attack = values
         
     def EXP(self, t0, duration, frec):
         values = np.zeros(duration)
-        for t in np.linspace(0, duration, frec):
+        for t in np.arange(0, duration, 1/frec):
             values[t] = np.exp(5 * (t - t0) / t0)
-        return values
-    
+        self.attack = values
+
     def QUARTSIN(self, t0, duration, frec):
         values = np.zeros(0, duration, frec)
-        for t in np.linspace(duration):
+        for t in np.arange(0, duration, 1/frec):
             values[t] = np.sin((pi * t) / (2 * t0))
-        return values
-    
+        self.attack = values
+
     def HALFSIN(self, t0, duration, frec):
         values = np.zeros(duration)
-        for t in np.linspace(duration):
+        for t in np.arange(0, duration, 1/frec):
             values[t] = (1 + np.cos(pi * (t / t0 - 0.5))) / 2
-        return values
-    
+        self.attack = values
+
     def LOG(self, t0, duration, frec):
         values = np.zeros(duration)
-        for t in np.linspace(duration):
+        for t in np.arange(0, duration, 1/frec):
             values[t] = np.log10(9 * t / t0 + 1)
-        return values
-    
+        self.attack = values
+
     def TRI(self, t0, t1, a1, duration, frec):
         values = np.zeros(duration)
-        for t in np.linspace(duration):
+        for t in np.arange(0, duration, 1/frec):
             if t < t1:
                 values[t] = t * a1 / t1
             elif t > t1:
                 values[t] = (t - t1) / (t1 - t0)
-        return values
-    
-    
-class Sustain():
-    
+        self.attack = values
+
     def CONSTANT(self, duration, frec):
-        return np.ones(duration)
-    
-    def INVLINEAR(self, t0, duration, frec):
+        self.sustain = np.ones(duration)
+
+    def INVLINEAR(self, t0, duration, frec, use: str):
         values = np.zeros(duration)
-        for t in np.linspace(duration):
+        for t in np.arange(0, duration, 1/frec):
             values[t] = (max(1 - (t / t0)))
-        return values
-    
+        if use == 'S':
+            self.sustain = values
+        elif use == 'D':
+            self.decay = values
+
     def SIN(self, a, f, duration, frec):
         values = np.zeros(duration)
-        for t in np.linspace(duration):
+        for t in np.arange(0, duration, 1/frec):
             values[t] = 1 + (a * np.sin(f * t))
-        return values
-    
-    def INVEXP(self, t0, duration, frec):
+        self.sustain = values
+
+    def INVEXP(self, t0, duration, frec, use):
         values = np.zeros(duration)
-        for t in np.linspace(duration):
+        for t in np.arange(0, duration, 1/frec):
             values[t] = np.exp((-5 * t) / t0)
-        return values
-    
-    def QUARTCOS(self, t0, duration, frec):
+        if use == 'S':
+            self.sustain = values
+        elif use == 'D':
+            self.decay = values
+
+    def QUARTCOS(self, t0, duration, frec, use):
         values = np.zeros(duration)
-        for t in np.linspace(duration):
+        for t in np.arange(0, duration, 1/frec):
             values[t] = np.cos((pi * t) / (2 * t0))
-        return values
-    
-    def HALFCOS(self, t0, duration, frec):
+        if use == 'S':
+            self.sustain = values
+        elif use == 'D':
+            self.decay = values
+
+    def HALFCOS(self, t0, duration, frec, use):
         values = np.zeros(duration)
-        for t in np.linspace(duration):
+        for t in np.arange(0, duration, 1/frec):
             values[t] = (1 + np.cos(pi * t / t0)) / 2
-        return values
-    
-    def INVLOG(self, t0, duration, frec):
+        if use == 'S':
+            self.sustain = values
+        elif use == 'D':
+            self.decay = values
+
+    def INVLOG(self, t0, duration, frec, use):
         values = np.zeros(duration)
-        for t in np.linspace(duration):
+        for t in np.arange(0, duration, 1/frec):
             if t < t0:
                 values[t] = np.log10((-9 * t) / t0 + 10)
             else:
                 values[t] = 0
-        return values
-    
+        if use == 'S':
+            self.sustain = values
+        elif use == 'D':
+            self.decay = values
+
     def PULSES(self, t0, t1, a1, duration, frec):
         return NotImplementedError
-    
-class Decay():
-    
-    def INVLINEAR(self, t0, duration, frec):
-        values = np.zeros(duration)
-        for t in np.linspace(duration):
-            values[t] = max(1 - (t / t0))
-        return values
-    
-    def INVEXP(self, t0, duration, frec):
-        values = np.zeros(duration)
-        for t in np.linspace(duration):
-            values[t] = np.exp((-5 * t) / t0)
-        return values
-    
-    def QUARTCOS(self, t0, duration, frec):
-        values = np.zeros(duration)
-        for t in np.linspace(duration):
-            values[t] = np.cos((pi * t) / (2 * t0))
-        return values
-    
-    def HALFCOS(self, t0, duration, frec):
-        values = np.zeros(duration)
-        for t in np.linspace(duration):
-            values[t] = (1 + np.cos(pi * t / t0)) / 2
-        return values
-    
-    def INVLOG(self, t0, duration, frec):
-        values = np.zeros(duration)
-        for t in np.linspace(duration):
-            if t < t0:
-                values[t] = np.log10((-9 * t) / t0 + 10)
-            else:
-                values[t] = 0
-        return values
--- functions.py
+++ functions.py
@@ -1,45 +1,45 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import asd
+from asdbis import Function
 from pathlib import Path
 
-def get_func(param: list, duration, fs):
-    if param[0] == 'CONSTANT':
-        func = asd.CONSTANT(duration, 1/fs)
-    elif param[0] == 'LINEAR':
-        func = asd.LINEAR(param[1], duration, 1/fs)
-    elif param[0] == 'INVLINEAR':
-        func = asd.INVLINEAR(param[1], duration, 1/fs)
-    elif param[0] == 'EXP':
-        func = asd.EXP(param[1], duration, 1/fs)
-    elif param[0] == 'INVEXP':
-        func = asd.INVEXP(param[1], duration, 1/fs)
-    elif param[0] == 'QUARTCOS':
-        func = asd.QUARTCOS(param[1], duration, 1/fs)
-    elif param[0] == 'QUARTSIN':
-        func = asd.QUARTSIN(param[1], duration, 1/fs)
-    elif param[0] == 'HALFCOS':
-        func = asd.HALFCOS(param[1], duration, 1/fs)
-    elif param[0] == 'HALFISN':
-        func = asd.HALFSIN(param[1], duration, 1/fs)
-    elif param[0] == 'LOG':
-        func = asd.LOG(param[1], duration, 1/fs)
-    elif param[0] == 'INVLOG':
-        func = asd.INVLOG(param[1], duration, 1/fs)
-    elif param[0] == 'SIN':
-        func = asd.SIN(param[1], param[2], duration, 1/fs)
-    elif param[0] == 'TRI':
-        func = asd.TRI(param[1], param[2], param[3], duration, 1/fs)
-    elif param[0] == 'PULSES':
-        func = asd.PULSES(param[1], param[2], param[3], duration, 1/fs)
-    else:
-        return ValueError
-    return func
+
+# def get_func(param: list, duration, fs):
+#     if param[0] == 'CONSTANT':
+#         func = asd.CONSTANT(duration, 1/fs)
+#     elif param[0] == 'LINEAR':
+#         func = asd.LINEAR(param[1], duration, 1/fs)
+#     elif param[0] == 'INVLINEAR':
+#         func = asd.INVLINEAR(param[1], duration, 1/fs)
+#     elif param[0] == 'EXP':
+#         func = asd.EXP(param[1], duration, 1/fs)
+#     elif param[0] == 'INVEXP':
+#         func = asd.INVEXP(param[1], duration, 1/fs)
+#     elif param[0] == 'QUARTCOS':
+#         func = asd.QUARTCOS(param[1], duration, 1/fs)
+#     elif param[0] == 'QUARTSIN':
+#         func = asd.QUARTSIN(param[1], duration, 1/fs)
+#     elif param[0] == 'HALFCOS':
+#         func = asd.HALFCOS(param[1], duration, 1/fs)
+#     elif param[0] == 'HALFISN':
+#         func = asd.HALFSIN(param[1], duration, 1/fs)
+#     elif param[0] == 'LOG':
+#         func = asd.LOG(param[1], duration, 1/fs)
+#     elif param[0] == 'INVLOG':
+#         func = asd.INVLOG(param[1], duration, 1/fs)
+#     elif param[0] == 'SIN':
+#         func = asd.SIN(param[1], param[2], duration, 1/fs)
+#     elif param[0] == 'TRI':
+#         func = asd.TRI(param[1], param[2], param[3], duration, 1/fs)
+#     elif param[0] == 'PULSES':
+#         func = asd.PULSES(param[1], param[2], param[3], duration, 1/fs)
+#     else:
+#         return ValueError
+#     return func
 
 def get_instrument(instrument) -> list:
     data = []
-    path = Path(instrument).parent / f"../instruments/{instrument}"
-    with open(path, 'r') as ins:
+    with open(f"instruments/{instrument}", 'r') as ins:
         for line in ins:
             line.rstrip('\n')
             info = line.split()
