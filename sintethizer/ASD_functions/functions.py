7afb606f6da6cf4cbe6e69aca4b67d25d00f11d0
ArcaloJade <102890895+ArcaloJade@users.noreply.github.com>
2022-06-16 12:03:09 -0300

    Add files via upload

--- sintethizer/ASD_functions/functions.py
+++ sintethizer/ASD_functions/functions.py
@@ -1,41 +1,42 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from asdbis import Function
-from pathlib import Path
 
 
-# def get_func(param: list, duration, fs):
-#     if param[0] == 'CONSTANT':
-#         func = asd.CONSTANT(duration, 1/fs)
-#     elif param[0] == 'LINEAR':
-#         func = asd.LINEAR(param[1], duration, 1/fs)
-#     elif param[0] == 'INVLINEAR':
-#         func = asd.INVLINEAR(param[1], duration, 1/fs)
-#     elif param[0] == 'EXP':
-#         func = asd.EXP(param[1], duration, 1/fs)
-#     elif param[0] == 'INVEXP':
-#         func = asd.INVEXP(param[1], duration, 1/fs)
-#     elif param[0] == 'QUARTCOS':
-#         func = asd.QUARTCOS(param[1], duration, 1/fs)
-#     elif param[0] == 'QUARTSIN':
-#         func = asd.QUARTSIN(param[1], duration, 1/fs)
-#     elif param[0] == 'HALFCOS':
-#         func = asd.HALFCOS(param[1], duration, 1/fs)
-#     elif param[0] == 'HALFISN':
-#         func = asd.HALFSIN(param[1], duration, 1/fs)
-#     elif param[0] == 'LOG':
-#         func = asd.LOG(param[1], duration, 1/fs)
-#     elif param[0] == 'INVLOG':
-#         func = asd.INVLOG(param[1], duration, 1/fs)
-#     elif param[0] == 'SIN':
-#         func = asd.SIN(param[1], param[2], duration, 1/fs)
-#     elif param[0] == 'TRI':
-#         func = asd.TRI(param[1], param[2], param[3], duration, 1/fs)
-#     elif param[0] == 'PULSES':
-#         func = asd.PULSES(param[1], param[2], param[3], duration, 1/fs)
-#     else:
-#         return ValueError
-#     return func
+def get_func(param: list, duration, fs, use):
+        func = Function()
+        if param[0] == 'CONSTANT':
+            func.CONSTANT(duration, fs, use)
+        elif param[0] == 'LINEAR':
+            func.LINEAR(param[1], duration, fs, use)
+        elif param[0] == 'INVLINEAR':
+            func.INVLINEAR(param[1], duration, fs, use)
+        elif param[0] == 'EXP':
+            func.EXP(param[1], duration, fs, use)
+        elif param[0] == 'INVEXP':
+            func.INVEXP(param[1], duration, fs, use)
+        elif param[0] == 'QUARTCOS':
+            func.QUARTCOS(param[1], duration, fs, use)
+        elif param[0] == 'QUARTSIN':
+            func.QUARTSIN(param[1], duration, fs, use)
+        elif param[0] == 'HALFCOS':
+            func.HALFCOS(param[1], duration, fs, use)
+        elif param[0] == 'HALFISN':
+            func.HALFSIN(param[1], duration, fs, use)
+        elif param[0] == 'LOG':
+            func.LOG(param[1], duration, fs, use)
+        elif param[0] == 'INVLOG':
+            func.INVLOG(param[1], duration, fs, use)
+        elif param[0] == 'SIN':
+            func.SIN(param[1], param[2], duration, fs, use)
+        elif param[0] == 'TRI':
+            func.TRI(param[1], param[2], param[3], duration, fs, use)
+        elif param[0] == 'PULSES':
+            func.PULSES(param[1], param[2], param[3], duration, fs, use)
+        else:
+            raise AssertionError('The given function is non-existant.')
+        return func
+    
 
 def get_instrument(instrument) -> list:
     data = []
