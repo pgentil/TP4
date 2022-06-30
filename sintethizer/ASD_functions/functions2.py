920fb8b9756aac1aa4ac10d041253747a4f1ab17
pgentil <pgentil@udesa.edu.ar>
2022-06-30 16:04:53 -0300

    push functions2 arreglos

--- sintethizer/ASD_functions/functions2.py
+++ sintethizer/ASD_functions/functions2.py
@@ -9,19 +9,19 @@ pi = np.pi
 
 def QUARTCOS(self, t0, array):
     note = np.zeros(len(array))
-    for t in len(array):
+    for t in range(len(array)):
         note[t] = np.cos((pi * array[t]) / (2 * t0))
     return note
 
 def HALFCOS(self, t0, array):
     note = np.zeros(len(array))
-    for t in len(array):
+    for t in range(len(array)):
         note[t] = (1 + np.cos(pi * t / t0)) / 2
     return note
 
 def INVLOG(self, t0, array):
     note = np.zeros(len(array))
-    for t in len(array):
+    for t in range(len(array)):
         if t < t0:
             note[t] = np.log10((-9 * t) / t0 + 10)
         else:
@@ -31,7 +31,7 @@ def INVLOG(self, t0, array):
 def PULSES(self, t0, t1, a1, array): ##NO FUNCIONA
     note = np.zeros(len(array))
     print(note)
-    for t in len(array):
+    for t in range(len(array)):
         tz = (array[t] / t0) - math.floor(array[t] / t0)
         print(tz)
         note[t] = min(abs(((1 - a1) / t1) * (tz - t0 + t1)) + a1)
