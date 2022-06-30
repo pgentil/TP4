e3a0c3c6e430281a5a560fab4a39df79d42552c1
pgentil <pgentil@udesa.edu.ar>
2022-06-30 16:35:14 -0300

    Update programa.py

--- sintethizer/ASD_functions/programa.py
+++ sintethizer/ASD_functions/programa.py
@@ -8,15 +8,22 @@ def main():
     duration_sustain = duration - duration_attack
     duration_decay = 1
     freq = 440
-    array = np.linspace(0, duration + duration_decay, freq *15)
     array_a = np.linspace(0, duration_attack, freq * 15)
     array_s = np.linspace(duration_attack, duration, freq *15)
-    array_d = np.linspace(duration, duration + duration_decay, freq*15)
+    array_d = np.linspace(0, duration_decay, freq * 15)
     function = Function()
 
     attack = function.EXP(duration_attack, array_a)
+    print (attack)
     sustain = function.CONSTANT(array_s)
-    decay = function.INVLINEAR(duration_decay, array_d)
+    print(sustain)
+    decay = function.INVEXP(duration_decay, array_d)
+    print(decay)
+
+    for t in range(len(array_d)):
+        array_d[t] += duration
+
+
 
     plt.plot(array_a, attack)
     plt.plot(array_s, sustain)
