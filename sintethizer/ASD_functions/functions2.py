b89ba3dc196120b60644374315e94fb3c1643e52
pgentil <pgentil@udesa.edu.ar>
2022-06-30 16:02:35 -0300

    functions 2

--- sintethizer/ASD_functions/functions2.py
+++ sintethizer/ASD_functions/functions2.py
@@ -2,48 +2,38 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
+from pyrsistent import T
+
 pi = np.pi
 
 
 def QUARTCOS(self, t0, array):
-    values = np.zeros(len(array))
+    note = np.zeros(len(array))
     for t in len(array):
-        while array[t] != 1:
-            pass
-        if array[t] == 1:
-            values[t] = np.cos((pi * array[t]) / (2 * t0))
-    return values
+        note[t] = np.cos((pi * array[t]) / (2 * t0))
+    return note
 
 def HALFCOS(self, t0, array):
-    values = np.zeros(len(array))
-    index = 0
+    note = np.zeros(len(array))
     for t in len(array):
-        if array[t] == 1:
-            values[index] = (1 + np.cos(pi * t / t0)) / 2
-    return values
+        note[t] = (1 + np.cos(pi * t / t0)) / 2
+    return note
 
-def INVLOG(self, t0, duration, frec):
-    sample = np.arange(0, duration + 1/frec, 1/frec)
-    values = np.zeros(len(sample))
-    index = 0
-    for t in sample:
+def INVLOG(self, t0, array):
+    note = np.zeros(len(array))
+    for t in len(array):
         if t < t0:
-            values[index] = np.log10((-9 * t) / t0 + 10)
-            index += 1
+            note[t] = np.log10((-9 * t) / t0 + 10)
         else:
-            values[index] = 0
-            index += 1
-    return values
+            note[t] = 0
+    return note
 
-def PULSES(self, t0, t1, a1, duration, frec): ##NO FUNCIONA
-    sample = np.arange(0, duration + 1/frec, 1/frec)
-    values = np.zeros(len(sample))
-    print(values)
-    index = 0
-    for t in sample:
-        tz = (t / t0) - math.floor(t / t0)
+def PULSES(self, t0, t1, a1, array): ##NO FUNCIONA
+    note = np.zeros(len(array))
+    print(note)
+    for t in len(array):
+        tz = (array[t] / t0) - math.floor(array[t] / t0)
         print(tz)
-        values[index] = min(abs(((1 - a1) / t1) * (tz - t0 + t1)) + a1)
-        print (values)
-        index += 1
-    return values
+        note[t] = min(abs(((1 - a1) / t1) * (tz - t0 + t1)) + a1)
+        print (note)
+    return note
