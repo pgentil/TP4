db198133d4cf55a70cb324d1f6ed1210dc207a1d
Fausti12 <fauspettinari@gmail.com>
2022-07-02 11:21:30 -0300

    Update functions.py

--- sintethizer/ASD_functions/functions.py
+++ sintethizer/ASD_functions/functions.py
@@ -7,36 +7,24 @@ pi = np.pi
 
 class Function():
     
-    def LINEAR(self, t0, array):
-        note = np.zeros(len(array))
-        for t in range(len(array)):
-            note = array[t] / t0
+    def LINEAR(self, t0, array):            
+        note = array / t0
         return note
         
     def EXP(self, t0, array):
-        #note = np.zeros(len(array))
-        #for t in range(len(array)):
-           # note[t] = np.exp(5 * (array[t] - t0) / t0)
-        
         note = np.exp(5 * (array - t0) / t0)
         return note
 
     def QUARTSIN(self, t0, array):
-        note = np.zeros(len(array))
-        for t in range(len(array)):
-            note[t] = np.sin((pi * array[t]) / (2 * t0))
+        note = np.sin((pi * array) / (2 * t0))
         return note
 
     def HALFSIN(self, t0, array):
-        note = np.zeros(len(array))
-        for t in len(array):
-            note[t] = (1 + np.cos(pi * (array[t] / t0 - 0.5))) / 2
+        note = (1 + np.cos(pi * (array / t0 - 0.5))) / 2
         return note
 
     def LOG(self, t0, array):
-        note = np.zeros(len(array))
-        for t in len(array):
-            note[t] = np.log10(9 * array[t] / t0 + 1)
+        note = np.log10(9 * array / t0 + 1)
         return note
 
     def TRI(self, t0, t1, a1, array):
@@ -46,38 +34,27 @@ class Function():
                 notes[t] = array[t] * a1 / t1
             elif t > t1:
                 notes[t] = (array[t] - t1) / (t1 - t0)
+        
         return notes
 
     def CONSTANT(self, array):
         return np.ones(len(array))
 
     def INVLINEAR(self, t0, array):
-        note = np.zeros(len(array))
-        #for t in range(len(array)):
-         #   note[t] = max((1 - (array[t] / t0), 0))
-        
         note = 1 - (array / t0)
         note[array < 0] = 0
         return note
 
     def SIN(self, a, f, array):
-        note = np.zeros(len(array))
-        #for t in range(len(array)):
-            #note[t] = 1 + (a * np.sin(f * array[t]))
-        
         note = 1 + (a * np.sin(f * array))
         return note
 
     def INVEXP(self, t0, array):
-        note = np.zeros(len(array))
-        for t in range(len(array)):
-            note[t] = np.exp((-5 * array[t]) / t0)
+        note = np.exp((-5 * array) / t0)
         return note
 
     def QUARTCOS(self, t0, array):
-        note = np.zeros(len(array))
-        for t in range(len(array)):
-            note[t] = np.cos((pi * array[t]) / (2 * t0))
+        note = np.cos((pi * array) / (2 * t0))
         return note
 
     def HALFCOS(self, t0, array):
@@ -111,16 +88,14 @@ class Function():
 if __name__ == "__main__":
     function1 = Function()
 
-    result = function1.INVLINEAR(3, np.array([1, 2, 3, 4, 5, 6]))
+    result = function1.EXP(5, np.array([1, 2, 3, 4, 5, 6]))
     print(result)
     x = np.linspace(0, 1, 6)
     plt.plot(x, result)
     plt.show()
 
     def liner(array, t0):
-        result = np.zeros(len(array))
-        for t in len(array):
-            result[t] = array[t] * (1/t0)
+        result = array  * (1/t0)
         return result
             
     
