72eae1cc340bb8beb4c73c3b105d558fd47f132c
pgentil <pgentil@udesa.edu.ar>
2022-07-05 15:14:51 -0300

    xylo

---
+++ r_client.py
@@ -0,0 +1,22 @@
+#/usr/bin/env python
+
+from xylophone.r_client import XyloClient
+from xylophone.xylo import XyloNote
+
+
+def main():
+        
+
+
+if __name__ == '__main__':
+    notes = [
+            XyloNote('A4', 1.5, 90),
+            XyloNote('A4', 2.3, 90),
+            XyloNote('G#6', 5.33333, 90),
+            XyloNote('A4', 10.01, 90),
+            ]
+
+    client = XyloClient(host='localhost', port=8080)
+    client.load(notes)
+    client.play()
+
