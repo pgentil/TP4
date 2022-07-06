46f054dd3e28f1ada5fce07f276554a205e511f5
pgentil <pgentil@udesa.edu.ar>
2022-07-05 21:17:14 -0300

    test_instrument

--- sintethizer/program/test_instrument.py
+++ sintethizer/program/test_instrument.py
@@ -12,10 +12,11 @@ class TestInstruments(unittest.TestCase):
         self.function = Function()
     
     def test_get_functions(self):
-        param = ['TRI', '0.05', '0.03', '1.3']
+        param = ['TRI', 0.05, 0.03, 1.3]
         array = np.array([1,2,3,4,5])
         func = self.instrument.get_functions(param, array)
-        self.assertEqual(func, self.function.TRI(0.05, 0.03, 1.3, array))
+        self.assertTrue(np.allclose(func, self.function.TRI(0.05, 0.03, 1.3, array)))
+        
 
 
     def tearDown(self) -> None:
