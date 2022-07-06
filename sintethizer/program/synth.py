c3ad27a1bf734979f528c21b960ee2bb715b9bca
ArcaloJade <102890895+ArcaloJade@users.noreply.github.com>
2022-07-05 23:13:16 -0300

    Update synth.py

--- sintethizer/program/synth.py
+++ sintethizer/program/synth.py
@@ -32,62 +32,26 @@ def read_scores(score_archive):
             scores.append(line)
         return scores
 
-
-def select_freq(read: list, i: int): #la frecuencia de una nota
+def complete_array(big_array, note: Notes, fs: int):
     '''
-    Selects a specific note's frequency by iterating over the notes_mapping list in the notes.py module and comparing it with the second element in the "read"
-    list of lists. If both elements are the same, the function returns its corresponding frequence.
+    Receives the song's array, a note and frequency. Adds a single note's full function into the song's array.
+    Returns the song array back again, but with said note's function in it.
     ---
-    read || list of lists. The bigger list can have an indefinite length, but each smaller list is supposed to have a length of 3, as it is supposed to be used
-    with what has been returned by the read_scores function.
-    i || int. This integer indicates which of the smaller lists contained in the "read" argument is going to be used.
+    big_array || np.array. It is the song's full array.
+    note || Note object. This object's function will be inserted into the song's array.
+    fs || int. This integer, representing the frequency, is used to multiply the song's start.
     '''
-    for n in range(len(notes.notes_mapping)):
-        if notes.notes_mapping[n][0] == read[i][1]:
-            return notes.notes_mapping[n][1]
-
-
-def create_func(freq: float, sample_rate: int, duration_note: float): #la funcio de la dicha nota 
-    t = np.arange(0, duration_note, 1/sample_rate)
-    one_array = 1 * np.sin(2 * np.pi * freq * t)
-    return one_array
-
-
-# def complete_array(leido, total_len, score_len, sample_rate): 
-#     big_array = np.zeros(int(total_len * sample_rate)+1) #a esto se le debe agregar las muestras del último decay
-#     print(f'Big array: {len(big_array)}')
-#     for n in range(score_len):
-#         freq = select_freq(leido, n)
-#         #print(freq)
-#         func = create_func(freq, sample_rate, float(leido[n][2]))
-#         start = int(float(leido[n][0]) * sample_rate)
-#         #print(start)
-#         end = len(func) + start 
-#         #print(end)
-        
-#         big_array[start:end] += func
-        
-#     return big_array
-
-def complete_array(big_array, note: Notes, fs: int, decay_duration): 
-    # print(f'Big array: {len(big_array)}')
-    # print(note.start)
-    # print(note.duration)
-    # print(note.soundwave)
-
-    # print(len(big_array[round(note.start * fs): round((note.start) * fs) + len(note.soundwave)]))
-    # print(len(note.soundwave))
     start = int(note.start * fs)
     big_array[start: start + len(note.soundwave)] += note.soundwave
-    # print(big_array) 
-    # for i in range(start, start+20):
-    #     print(big_array[i])
     return big_array
 
-
-
-
 def song_duration(leido):
+    '''
+    Calculates the duration of the whole song in terms of seconds.
+    Returns the calculated duration.
+    ---
+    leido || list of lists. Each list contains three elements: start, note, duration. Only the start and note elements are used in this function.
+    '''
     total_len = 0
     for i in range(len(leido)):
         duration = float(leido[i][0]) + float(leido[i][2])
@@ -95,34 +59,7 @@ def song_duration(leido):
             total_len = duration
     return total_len
 
-
-
-def create_song(archive_name, sample_rate, array_song):
-    """
-    Creates a wavefile
-    Parameters
-    ----------
-    archive_name : String
-        The archive's name.
-    sample_rate : Int
-        Generally 44100.
-    array_song : Array
-        Array which contains the functions values.
-
-    """
-    write(archive_name, sample_rate, array_song)
-
 if __name__ == "__main__":
+    pass
 
-
-    leido = read_scores('debussy-clair-de-lune.txt')
-    # print(leido)
-    score_len = len(leido)
-    #print(score_len)
-    total_len = song_duration(leido)
-    # print(total_len)
-    song = complete_array(leido, total_len, score_len, 44100)
-
-
-
-    sample_rate = 44100
+    #sample_rate = 44100
